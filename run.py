10ca0c1c07c1d8915fcdaf0c1975728d1fe6b03a
Chris <christopherryan91@gmail.com>
2023-08-15 17:24:22 +0100

    get data validation working

--- run.py
+++ run.py
@@ -21,9 +21,21 @@ def get_sales_data():
     print("Data should be six numbers")
     print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here")
+    data_str = input("Enter your data here: ")
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
 
-    print(f"The data provided is {data_str}")
+def validate_data(values):
+
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"Provide six values rather than {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again\n")
+
 
 get_sales_data()
 
