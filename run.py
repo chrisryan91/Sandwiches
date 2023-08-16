5b35e3db9cf1930283137572f20728b30ce42d3d
Chris <christopherryan91@gmail.com>
2023-08-16 21:16:28 +0100

    add function to calculate_surplus_data and print the last row of the stock spreadsheet

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
-
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,15 +12,15 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('sandwiches')
-    
+
+
 def get_sales_data():
     """
-    Get sales figures from user
+    Get sales figures input from the user.
     """
     while True:
-
-        print("Please enter sales data")
-        print("Data should be six numbers")
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
@@ -28,21 +28,58 @@ def get_sales_data():
         sales_data = data_str.split(",")
 
         if validate_data(sales_data):
-            print("data is valid")
+            print("Data is valid!")
             break
 
-def validate_data(values):
+    return sales_data
 
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
     try:
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f"Provide six values rather than {len(values)}"
+                f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
-        print(f"Invalid data: {e}, please try again\n")
+        print(f"Invalid data: {e}, please try again.\n")
         return False
-    
+
     return True
 
-data = get_sales_data()
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet
+    """
+    print("updating sales worksheet...\n")#
+    sales_worksheet = SHEET.worksheet('sales')
+    sales_worksheet.append_row(data)
+    print("sales work sheet updated")
+
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item    
+    """
+    print("Calculate surplus data... \n")
+    stock = SHEET.worksheet('stock').get_all_values()
+    last_row = stock[-1]
+    print(last_row)
+
+def main():
+    """
+    Run all program functions
+    """
+
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+print("Welcome to Love Sandwiches Data Automation")
+
+main()
